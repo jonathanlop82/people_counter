c02b768740b47c494bae973ba83a1ac05018b391
Tecnologia <jonathanlop82@gmail.com>
2021-11-08 12:37:32 -0500

    Update main file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import cv2
+import numpy as np
+
 def run():
-    pass
+    cap = cv2.VideoCapture('rtsp://altaplaza:62741797@10.0.0.91/H264?ch=1&subtype=0')
+
+    while(True):
+        ret, frame = cap.read()
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
